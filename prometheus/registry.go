077902ef70c295b5760a01a27681448129ae84e9
70data <70data@gmail.com>
2018-12-02 23:29:29 +0800

    add example

--- prometheus/registry.go
+++ prometheus/registry.go
@@ -30,7 +30,7 @@ import (
 
 	dto "github.com/prometheus/client_model/go"
 
-	"github.com/prometheus/client_golang/prometheus/internal"
+	"github.com/70data/golang-prometheus/prometheus/internal"
 )
 
 const (
